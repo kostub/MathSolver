a0606e264d7ef5553bfc2b21474194f45b061f0b
Kostub Deshmukh <kostub@gmail.com>
2016-05-26 11:21:46 -0700

    Initial commit.

---
+++ MathSolverTests/MathSolverTests.m
@@ -0,0 +1,39 @@
+//
+//  MathSolverTests.m
+//  MathSolverTests
+//
+//  Created by Kostub Deshmukh on 5/26/16.
+//  Copyright © 2016 Kostub Deshmukh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MathSolverTests : XCTestCase
+
+@end
+
+@implementation MathSolverTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
