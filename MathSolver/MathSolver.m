c3af7723176b09d20105a4ec232118c937197fb9
Kostub Deshmukh <kostub@gmail.com>
2016-05-26 11:16:09 -0700

    Initial Commit

---
+++ MathSolver/MathSolver.m
@@ -0,0 +1,13 @@
+//
+//  MathSolver.m
+//  MathSolver
+//
+//  Created by Kostub Deshmukh on 5/26/16.
+//  Copyright © 2016 Kostub Deshmukh. All rights reserved.
+//
+
+#import "MathSolver.h"
+
+@implementation MathSolver
+
+@end
